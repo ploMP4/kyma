5b7a09261a4dbb09a8a06b64497f9c6c6a8b7c77
Konstantinos Artopoulos <artopouloskon@gmail.com>
2025-04-03 23:40:13 +0300

    feat: slide left transition + random page flip transition

--- internal/tui/transition.go
+++ internal/tui/transition.go
@@ -7,7 +7,10 @@ import (
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/harmonica"
+	"github.com/muesli/reflow/truncate"
+	"github.com/muesli/reflow/wordwrap"
 
+	"github.com/ploMP4/orama/internal/skip"
 	"github.com/ploMP4/orama/internal/tui/messages"
 )
 
@@ -160,6 +163,144 @@ func (t verticalSlideDownTransition) View(prev, next string) string {
 		y = len(prevLines)
 	}
 	s.WriteString("\n" + strings.Join(prevLines[:len(prevLines)-y], "\n"))
+
+	return s.String()
+}
+
+type horizontalSlideLeftTransition struct {
+	width     int
+	fps       int
+	spring    harmonica.Spring
+	x         float64
+	xVel      float64
+	animating bool
+}
+
+func NewHorizontalSlideLeftTransition(fps int) horizontalSlideLeftTransition {
+	const frequency = 7.0
+	const damping = 0.8
+
+	return horizontalSlideLeftTransition{
+		fps:    fps,
+		spring: harmonica.NewSpring(harmonica.FPS(fps), frequency, damping),
+	}
+}
+
+func (t horizontalSlideLeftTransition) Start(width int, _ int) Transition {
+	t.width = width
+	t.animating = true
+	t.x = 0
+	t.xVel = 0
+	return t
+}
+
+func (t horizontalSlideLeftTransition) Animating() bool {
+	return t.animating
+}
+
+func (t horizontalSlideLeftTransition) Update() (Transition, tea.Cmd) {
+	targetX := float64(t.width)
+
+	t.x, t.xVel = t.spring.Update(t.x, t.xVel, targetX)
+
+	if t.x >= targetX {
+		t.animating = false
+		return t, nil
+	}
+
+	return t, messages.Animate(time.Duration(t.fps))
+}
+
+func (t horizontalSlideLeftTransition) View(prev string, next string) string {
+	var s strings.Builder
+
+	x := int(math.Round(t.x))
+
+	prevLines := strings.Split(prev, "\n")
+	nextLines := strings.Split(next, "\n")
+
+	// assert that slides are always equal size
+	if len(nextLines) != len(prevLines) {
+		panic("Slides of not equal height")
+	}
+
+	for i := range nextLines {
+		prev := skip.String(prevLines[i], uint(x))
+		next := truncate.String(nextLines[i], uint(x))
+		s.WriteString(prev + " " + next)
+		if i < len(nextLines)-1 {
+			s.WriteString("\n")
+		}
+	}
+	return s.String()
+}
+
+type pageFlipRightTransition struct {
+	width     int
+	fps       int
+	spring    harmonica.Spring
+	x         float64
+	xVel      float64
+	animating bool
+}
+
+func NewPageFlipRightTransition(fps int) pageFlipRightTransition {
+	const frequency = 7.0
+	const damping = 0.8
+
+	return pageFlipRightTransition{
+		fps:    fps,
+		spring: harmonica.NewSpring(harmonica.FPS(fps), frequency, damping),
+	}
+}
+
+func (t pageFlipRightTransition) Start(width int, _ int) Transition {
+	t.width = width
+	t.animating = true
+	t.x = 0
+	t.xVel = 0
+	return t
+}
+
+func (t pageFlipRightTransition) Animating() bool {
+	return t.animating
+}
+
+func (t pageFlipRightTransition) Update() (Transition, tea.Cmd) {
+	targetX := float64(t.width)
+
+	t.x, t.xVel = t.spring.Update(t.x, t.xVel, targetX)
+
+	if t.x >= targetX {
+		t.animating = false
+		return t, nil
+	}
+
+	return t, messages.Animate(time.Duration(t.fps))
+}
+
+func (t pageFlipRightTransition) View(prev string, next string) string {
+	var s strings.Builder
+
+	x := int(math.Round(t.x))
+
+	prevLines := strings.Split(prev, "\n")
+	nextLines := strings.Split(next, "\n")
+
+	// assert that slides are always equal size
+	if len(nextLines) != len(prevLines) {
+		panic("Slides of not equal height")
+	}
+
+	for i := range nextLines {
+		wrappedPrev := strings.Split(wordwrap.String(prevLines[i], x), "\n")
+		prev = wrappedPrev[len(wrappedPrev)-1]
+		next := truncate.String(nextLines[i], uint(x))
+		s.WriteString(next + " " + prev)
+		if i < len(nextLines)-1 {
+			s.WriteString("\n")
+		}
+	}
 
 	return s.String()
 }
