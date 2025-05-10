07950000417f4277bdb2d02b3fb28b630a4963bd
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-05-10 17:52:58 +0300

    feat(tui): add directional support to slide navigation
    - add `transitions` import
    - pass direction parameter to transition `Start()` method
    - use forward direction for next slide navigation
    - use backward direction with opposite transition for previous slide navigation

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -7,6 +7,7 @@ import (
 	"github.com/charmbracelet/lipgloss"
 
 	"github.com/ploMP4/kyma/internal/tui/messages"
+	"github.com/ploMP4/kyma/internal/tui/transitions"
 )
 
 type keyMap struct {
@@ -92,13 +93,15 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				return m, nil
 			}
 			m.slide = m.slide.Next
-			m.slide.Properties.Transition = m.slide.Properties.Transition.Start(m.width, m.height)
+			m.slide.Properties.Transition = m.slide.Properties.Transition.Start(m.width, m.height, transitions.Forwards)
 			return m, messages.Animate(fps)
 		} else if key.Matches(msg, m.keys.Prev) {
 			if m.slide.Prev == nil || m.slide.Properties.Transition.Animating() {
 				return m, nil
 			}
+			transition := transitions.Get(m.slide.Properties.Transition.Name(), fps).Opposite()
 			m.slide = m.slide.Prev
+			m.slide.Properties.Transition = transition.Start(m.width, m.height, transitions.Backwards)
 			return m, messages.Animate(fps)
 		}
 	case messages.FrameMsg:
